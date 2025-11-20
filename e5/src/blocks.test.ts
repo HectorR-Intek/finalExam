e44bb5e4e3eb1fe958ed96a33bcbfc94fdbcac36
HectorR-Intek <hjrodriguez@intekglobal.com>
2025-11-20 16:10:23 -0600

    Add files via upload

--- e5/src/blocks.test.ts
+++ e5/src/blocks.test.ts
@@ -1,31 +1,41 @@
 import { reverseBlocks } from "./blocks";
 
-test("reverseBlocks returns error if blockSize is greater than arrays length", () => {
-  expect(reverseBlocks([0, 1, 2], 4)).toBe(
-    "blockSize has to be lesser than the array's length"
+describe("Function reverseBlocks", () => {
+  it("reverseBlocks returns error if blockSize is greater than arrays length", () => {
+    expect(() => reverseBlocks([0, 1, 2], 4)).toThrow(
+      "blockSize has to be lesser than the array's length"
+    );
+  });
+
+  it.each([
+    { arr: [0, 1], blockSize: 2, expected: [1, 0] },
+    { arr: [0, 1, 2, 3, 4], blockSize: 3, expected: [2, 1, 0, 3, 4] },
+    { arr: [0, 1, 2, 3, 4, 5], blockSize: 3, expected: [2, 1, 0, 5, 4, 3] },
+    { arr: [0, 1, 2], blockSize: 1, expected: [0, 1, 2] },
+    { arr: [0, 1, 2], blockSize: 3, expected: [2, 1, 0] },
+    { arr: [1], blockSize: 1, expected: [1] },
+  ])(
+    "reverseBlocks performs succesfully for different positive values of blockSize",
+    ({ arr, blockSize, expected }) => {
+      expect(reverseBlocks(arr, blockSize)).toStrictEqual(expected);
+    }
+  );
+
+  it.each([
+    {
+      arr: [],
+      blockSize: 0,
+      expected: "blockSize has to be greater than zero",
+    },
+    {
+      arr: [],
+      blockSize: -5,
+      expected: "blockSize has to be greater than zero",
+    },
+  ])(
+    "reverseBlocks returns error for non-positive values of blockSize",
+    ({ arr, blockSize, expected }) => {
+      expect(() => reverseBlocks(arr, blockSize)).toThrow(expected);
+    }
   );
 });
-
-test.each([
-  { arr: [0, 1], blockSize: 2, expected: [1, 0] },
-  { arr: [0, 1, 2, 3, 4], blockSize: 3, expected: [2, 1, 0, 3, 4] },
-  { arr: [0, 1, 2, 3, 4, 5], blockSize: 3, expected: [2, 1, 0, 5, 4, 3] },
-  { arr: [0, 1, 2], blockSize: 1, expected: [0, 1, 2] },
-  { arr: [0, 1, 2], blockSize: 3, expected: [2, 1, 0] },
-  { arr: [1], blockSize: 1, expected: [1] },
-])(
-  "reverseBlocks performs succesfully for different positive values of blockSize",
-  ({ arr, blockSize, expected }) => {
-    expect(reverseBlocks(arr, blockSize)).toStrictEqual(expected);
-  }
-);
-
-test.each([
-  { arr: [], blockSize: 0, expected: "blockSize has to be greater than zero" },
-  { arr: [], blockSize: -5, expected: "blockSize has to be greater than zero" },
-])(
-  "reverseBlocks returns error for non-positive values of blockSize",
-  ({ arr, blockSize, expected }) => {
-    expect(reverseBlocks(arr, blockSize)).toBe(expected);
-  }
-);
--- e5/src/blocks.ts
+++ e5/src/blocks.ts
@@ -1,28 +1,33 @@
-export function reverseBlocks<T>(arr: T[], blockSize: number) {
+export function reverseBlocks<T extends {}>(arr: T[], blockSize: number) {
   if (arr.length < blockSize) {
-    return "blockSize has to be lesser than the array's length";
+    throw new Error("blockSize has to be lesser than the array's length");
   }
   if (blockSize <= 0) {
-    return "blockSize has to be greater than zero";
+    throw new Error("blockSize has to be greater than zero");
   }
 
   if (arr.length == blockSize) {
     let temp;
     let last = arr.length - 1;
     for (let i = 0; i < arr.length; i++) {
-      temp = arr[i];
-      arr[i] = arr[last];
+      temp = arr[i] as T;
+      arr[i] = arr[last] as T;
       arr[last] = temp;
       last--;
     }
   }
 
   let blocks = 0;
+
   while (arr.length - blocks * blockSize >= blockSize) {
-    let temp = arr.slice(blocks * blockSize, blockSize * (blocks + 1));
-    for (let i = 0; i < temp.length; i++) {
-      let index = blockSize * (blocks + 1) - (i + 1);
-      arr[index] = temp[i];
+    let indecs = blocks * blockSize;
+    let temp;
+    let last = indecs + blockSize - 1;
+    for (let j = indecs; j < indecs + blockSize - 1; j++) {
+      temp = arr[j];
+      arr[j] = arr[last] as T;
+      arr[last] = temp as T;
+      last--;
     }
     blocks++;
   }
