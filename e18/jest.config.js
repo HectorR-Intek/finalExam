8bdaf7ca4b62201a7d5a03b05255e9181d57c0ef
HectorR-Intek <hjrodriguez@intekglobal.com>
2025-11-20 16:15:19 -0600

    Add files via upload

--- e18/jest.config.js
+++ e18/jest.config.js
@@ -5,6 +5,7 @@ const tsJestTransformCfg = createDefaultPreset().transform;
 /** @type {import("jest").Config} **/
 module.exports = {
   testEnvironment: "node",
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/dist/"],
   transform: {
     ...tsJestTransformCfg,
   },
